64f4218b49d8645d13d4f1a732876ff3b7e1e5dd
ashley <ashleylai58@gmail.com>
2022-12-15 18:57:13 +0800

    test medium format

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import cheerio from 'cheerio';
 require('dotenv').config();
 
 const FOLLOWERS_COUNT_REGEX = /[\d+*\.*A-Z?]*\sFollowers/;
-const CLAPS_COUNT_REGEX = /\d+(\.\d{1,2})?K?\s?/;
+// const CLAPS_COUNT_REGEX = /\d+(\.\d{1,2})?K?\s?/;
+const CLAPS_COUNT_REGEX = /clapCount":(.*?),/;
 const MAX_STR_LENGTH = 30;
 const MEDIUM_API_BASE_URL =
   'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@';
@@ -42,16 +43,20 @@ interface APIResponse {
     // Use cheerio to get articles' claps count
     slicedData = await Promise.all(
       apiResponse.data.items
-        .filter(item => item.categories.length !== 0) // filter comment
-        .slice(0, 3) // latest 3 articles
-        .map(async item => {
+        .filter((item) => item.categories.length !== 0) // filter comment
+        .map(async (item) => {
           const res = await axios.get(item.guid);
-          const $ = cheerio.load(res.data);
-          const text = $('div.pw-multi-vote-count button').first().text();
-          let matches = text.match(CLAPS_COUNT_REGEX);
-          return { title: item.title, claps: matches ? matches[0] : '0' };
+          // const $ = cheerio.load(res.data);
+          // const text = $('div.pw-multi-vote-count button').first().text();
+          let matches = res.data.match(CLAPS_COUNT_REGEX);
+          return { title: item.title, claps: matches ? matches[1] : '0' };
         })
     );
+    slicedData = slicedData
+      .sort((a, b) => {
+        return parseInt(b.claps, 10) - parseInt(a.claps, 10);
+      })
+      .slice(0, 3);
   } catch (err) {
     throw new Error('Get data failed');
   }
@@ -66,7 +71,7 @@ interface APIResponse {
     ? followerCountMatchList[0]
     : '??? Followers';
 
-  slicedData.forEach(item => {
+  slicedData.forEach((item) => {
     let trimTitle;
     if (item.title.length > MAX_STR_LENGTH)
       trimTitle = item.title.slice(0, MAX_STR_LENGTH) + '...';
@@ -87,11 +92,13 @@ interface APIResponse {
     { align: ['l', 'r'], stringLength: () => 20 }
   );
 
-  const box = new GistBox({ id: GIST_ID, token: GH_PAT });
+  console.log(gistContent);
 
-  try {
-    await box.update({ filename: 'medium-stat.md', content: gistContent });
-  } catch (err) {
-    throw new Error('Update gist failed');
-  }
+  // const box = new GistBox({ id: GIST_ID, token: GH_PAT });
+
+  // try {
+  //   await box.update({ filename: 'medium-stat.md', content: gistContent });
+  // } catch (err) {
+  //   throw new Error('Update gist failed');
+  // }
 })();
